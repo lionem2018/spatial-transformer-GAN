bcc4c9e26982c3d04d1336d0ad6c203012704b58
lionem2018 <lionem2018@gmail.com>
2019-05-12 09:22:18 +0900

    modify networks

--- glasses/data.py
+++ glasses/data.py
@@ -15,6 +15,7 @@ def load(opt, test=False):
 	# 불러올 디렉토리 경로 설정
 	main_folder = "/content/gdrive/My Drive/Colab Notebooks/spatial-transformer-GAN/glasses/"
 	path = main_folder + "dataset"
+	"""
 	# 테스트 데이터셋를 불러와야 한다면,
 	if test:
 		# 테스트 이미지 데이터가 담긴 .npy 파일 불러오기
@@ -27,16 +28,14 @@ def load(opt, test=False):
 
 	# 학습 데이터셋을 불러와야 한다면,
 	else:
-	# 	# 학습 이미지 데이터가 담긴 .npy 파일 불러오기
-	# 	images = np.load("{0}/image_train.npy".format(path))
-	# 	# 안경 유무 속성 정보 불러오기
-	# 	hasGlasses = np.load("{0}/attribute_train.npy".format(path))[:, 15]
 		images_0 = np.load("{0}/not_wearing_earring_test.npy".format(path))  # 귀걸이 안 낀 이미지 npy 파일
 		images_1 = np.load("{0}/earring_image_test.npy".format(path))  # 귀걸이 낀 이미지 npy 파일
+	"""
 
-	# # 안경 유무에 따라 이미지 데이터셋 나누기
-	# images_0 = images[~hasGlasses]
-	# images_1 = images[hasGlasses]
+	# 테스트 이미지 데이터가 담긴 .npy 파일 불러오기
+	images = np.load("{0}/image_test.npy".format(path))  # 귀걸이 안 낀 이미지 npy 파일
+	# 귀걸이 착용 유무 속성정보 불러오기
+	hasGlasses = np.load("{0}/attribute_test.npy".format(path))[:, 35]
 
 	# 안경 데이터셋 불러오기
 	glasses = np.load("{0}/earring.npy".format(path))
--- glasses/train_Donly.py
+++ glasses/train_Donly.py
@@ -75,8 +75,8 @@ with tf.device(opt.GPUdevice):
 # load data
 print(util.toMagenta("loading training data..."))
 trainData = data.load(opt)
-print(util.toMagenta("loading test data..."))
-testData = data.load(opt,test=True)
+# print(util.toMagenta("loading test data..."))
+# testData = data.load(opt,test=True)
 
 # prepare model saver/summary writer
 saver_D = tf.train.Saver(var_list=varsD,max_to_keep=20)
@@ -118,19 +118,19 @@ with tf.Session(config=tfConfig) as sess:
 						util.toRed("X"),
 						util.toRed("{0:.4f}".format(ld)),
 						util.toBlue("{0:.4f}".format(gdn))))
-		if (i+1)%20==0:
-			runList = [summaryLossTrain,summaryGradTrain]
-			sl,sg = sess.run(runList,feed_dict=batch)
-			summaryWriter.add_summary(sl,i+1)
-			summaryWriter.add_summary(sg,i+1)
-		if (i+1)%200==0:
-			si = sess.run(summaryImageTrain,feed_dict=batch)
-			summaryWriter.add_summary(si,i+1)
-		if (i+1)%500==0:
-			# run on test set
-			batch = data.makeBatch(opt,testData,PH)
-			si = sess.run(summaryImageTest,feed_dict=batch)
-			summaryWriter.add_summary(si,i+1)
+		# if (i+1)%20==0:
+		# 	runList = [summaryLossTrain,summaryGradTrain]
+		# 	sl,sg = sess.run(runList,feed_dict=batch)
+		# 	summaryWriter.add_summary(sl,i+1)
+		# 	summaryWriter.add_summary(sg,i+1)
+		# if (i+1)%200==0:
+		# 	si = sess.run(summaryImageTrain,feed_dict=batch)
+		# 	summaryWriter.add_summary(si,i+1)
+		# if (i+1)%500==0:
+		# 	# run on test set
+		# 	batch = data.makeBatch(opt,testData,PH)
+		# 	si = sess.run(summaryImageTest,feed_dict=batch)
+		# 	summaryWriter.add_summary(si,i+1)
 		if (i+1)%1000==0:
 			# save model
 			util.saveModel(opt,sess,saver_D,"D",i+1)
