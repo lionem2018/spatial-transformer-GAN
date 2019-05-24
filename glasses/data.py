5933b52c127e58c0f2f7a6e472e31da98356e606
lionem2018 <lionem2018@gmail.com>
2019-05-25 00:22:28 +0900

    modify data.py

--- glasses/data.py
+++ glasses/data.py
@@ -28,8 +28,12 @@ def load(opt, test=False):
 
 	# 학습 데이터셋을 불러와야 한다면,
 	else:
-		images_0 = np.vstack(np.load("{0}/not_wearing_earing_test_0.npy".format(path)), np.load("{0}/not_wearing_earing_test_1.npy".format(path)))  # 귀걸이 안 낀 이미지 npy 파일
-		images_1 = np.vstack(np.load("{0}/earing_image_test_0.npy".format(path)), np.load("{0}/earing_image_test_1.npy".format(path)))  # 귀걸이 낀 이미지 npy 파일
+		not_wearing_images_0 = np.load("{0}/not_wearing_earing_test_0.npy".format(path))
+		not_wearing_images_1 = np.load("{0}/not_wearing_earing_test_1.npy".format(path))
+		wearing_images_0 = np.load("{0}/earing_image_test_0.npy".format(path))
+		wearing_images_1 = np.load("{0}/earing_image_test_1.npy".format(path))
+		images_0 = np.vstack([not_wearing_images_0, not_wearing_images_1])  # 귀걸이 안 낀 이미지 npy 파일
+		images_1 = np.vstack([wearing_images_0, wearing_images_1])  # 귀걸이 낀 이미지 npy 파일
 	"""
 
 	# 테스트 이미지 데이터가 담긴 .npy 파일 불러오기
